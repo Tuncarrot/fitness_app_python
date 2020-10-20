2c3a57edf55ed7832edf07c8f4211926e87c4a37
Adam Tunkiewicz <Atunkiewicz0881@conestogac.on.ca>
2020-10-19 21:18:25 -0400

    :construction: ripple button effect WIP

--- fitnessAppFlask/static/js/home-interactions.js
+++ fitnessAppFlask/static/js/home-interactions.js
@@ -1,3 +1,21 @@
+const buttons = document.querySelectorAll('a');
+buttons.forEach(btn => {
+    btn.addEventListener('click', function(e) {
+        
+        let x = e.clientX - e.target.offsetLeft;
+        let y = e.clientY - e.target.offsetTop;
+
+        let ripples = document.createElement('span');
+        ripples.style.left = x + 'px';
+        ripples.style.top = x + 'px';
+        this.appendChild(ripples);
+
+        setTimeout(() => {
+            ripples.remove()
+            }, 1000);
+    })
+})
+
 function switchLayout() {
     var icon = document.getElementById("layout-icon");
     var para = document.getElementById("layout-paragraph");
@@ -9,3 +27,4 @@ function switchLayout() {
         para.style.display = "block";
     }
   } 
+
